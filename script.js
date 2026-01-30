d75d5951dd8de95f24796f05f8c02b1b57d6c590
illia <smahliukillia927@gmail.com>
2026-01-30 19:18:45 +0100

    DOOM Manipulation

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+let buttonContainer = document.querySelector("#button-container");
+let screen = document.querySelector("#result");
+let playerChoice = null;
+
+let start = document.querySelector("#start");
+
+buttonContainer.addEventListener("click", (event) => {
+  let target = event.target;
+  switch (target.id) {
+    case "rock-button":
+      playerChoice = 0;
+      break;
+    case "paper-button":
+      playerChoice = 1;
+      break;
+    case "scissors-button":
+      playerChoice = 2;
+      break;
+  }
+});
+
 let humanScore = 0;
 let computerScore = 0;
+let draw = 0;
 
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
@@ -7,32 +29,24 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("0 = rock, 1 = paper, 2 = scissors");
-  console.log(humanChoice);
-  return humanChoice;
-}
-
 function playRound(first, second) {
   if (first == second) {
-    return console.log("Draw");
+    return (console.log("Draw"), draw++);
   } else if (first == 0 && second == 2) {
-    return (console.log("human win"), (humanScore += 1));
+    return (console.log("human win"), humanScore++);
   } else if (second == 0 && first == 2) {
-    return (console.log("computer win"), (computerScore += 1));
+    return (console.log("computer win"), computerScore++);
   } else if (first < second) {
-    return (console.log("computer win"), (computerScore += 1));
+    return (console.log("computer win"), computerScore++);
   } else if (first > second) {
-    return (console.log("human win"), (humanScore += 1));
+    return (console.log("human win"), humanScore++);
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  console.log(humanScore);
-  console.log(computerScore);
-}
-
-playGame();
+start.addEventListener("click", () => {
+  playRound(playerChoice, getComputerChoice());
+  screen.textContent =
+    "human: " + humanScore + " computer: " + computerScore + " Draw:" + draw;
+});
+screen.textContent =
+  "human: " + humanScore + " computer: " + computerScore + " Draw:" + draw;
